7111dcc5b17806fb8c4e9058e617e6271f8e7ec4
Tohru Ohno <daye001@gmail.com>
2014-04-30 14:23:26 +0900

    Finish demo_app

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,7 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
-  validate :content, :length => { :maximum => 140}
+  
+  belongs_to :user
+  
+  validates :content, :length => { :maximum => 140}
 end
